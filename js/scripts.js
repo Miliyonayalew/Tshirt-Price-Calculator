0b7c49285c8d5769ff1a3454346c14f7c30abae9
Miliyon Ayalew <35680940+Miliyonayalew@users.noreply.github.com>
2021-10-21 22:14:34 +0300

    First Commit

---
+++ js/scripts.js
@@ -0,0 +1,65 @@
+
+var products = {
+    'white': {
+        
+        'plain': {
+            'unit_price': 5.12,
+            'photo': 'v-white.jpg' 
+        },
+        'printed': {
+            'unit_price': 8.95,
+            'photo': 'v-white-personalized.jpg' 
+        }
+    },
+    
+    'colored': {
+        'plain': {
+            'unit_price': 6.04,
+            'photo': 'v-color.jpg' 
+        },
+        'printed': {
+            'unit_price': 9.47,
+            'photo': 'v-color-personalized.png' 
+        }
+    }
+}
+
+
+// Search params
+
+var search_params = {
+    "quantity": "",
+    "color": "",
+    "quality": "",
+    "style": "",
+}
+
+
+// Additional pricing rules:
+
+// 1. The prices above are for Basic quality (q150). 
+// The high quality shirt (190g/m2) has a 12% increase in the unit price.
+
+// 2. Apply the following discounts for higher quantities: 
+    // 1: above 1.000 units - 20% discount
+    // 2: above 500 units - 12% discount
+    // 3: above 100 units - 5% discount
+
+
+// Solution:
+
+$(function(){
+
+  
+    
+});
+
+
+
+
+
+
+
+
+
+
